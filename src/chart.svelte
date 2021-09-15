b4bb21073715e91878af628a31b2650fdde3c6ca
Justin Rokisky <jrokisky831@gmail.com>
2021-09-15 09:11:41 -0400

    Copied over scott logic 's example annotation

--- src/chart.svelte
+++ src/chart.svelte
@@ -1,7 +1,9 @@
 <script>
-    import { onMount, setContext } from 'svelte'
-    import { seriesWebglPoint, chartCartesian, seriesWebglMulti, webglFillColor } from "d3fc"
-    import { zoom, scaleLinear, zoomIdentity, zoomTransform } from "d3"
+    import { onMount } from 'svelte'
+    import { seriesWebglPoint, chartCartesian, seriesWebglMulti, webglFillColor, pointer, seriesSvgMulti } from "d3fc"
+    import { zoom, scaleLinear, zoomIdentity } from "d3"
+    import { annotationCallout } from 'd3-svg-annotation'
+    import { seriesSvgAnnotation } from './annotation-series'
     import * as d3 from 'd3'
 
     import points from './point-generator'
@@ -14,22 +16,35 @@
 
     let data = []
     let filtered_data = []
+    const annotations = []
 
     let filter_val = 0
+    let quadtree
 
-    const redraw = () => {
-        d3.select("#canvas").datum({ filtered_data }).call(chart);
-    }
 
     $: {
         filter_val
         filtered_data = applyFilters()
+        buildQuadTree()
         setFills()
         redraw()
     }
 
+    // Data ops.
     function applyFilters() {
         return data.filter(x => x.tc > filter_val/100)
+    }
+
+    const setFills = () => {
+        const fillColor = webglFillColor().value(x => webglColor(x.color)).data(filtered_data)
+        pointSeries.decorate(program => fillColor(program))
+    }
+
+    const buildQuadTree = () => quadtree = d3.quadtree().x(d => d.x).y(d => d.y).addAll(filtered_data)
+
+    /// D3FC data and charting
+    const redraw = () => {
+        d3.select("#canvas").datum({ annotations, filtered_data }).call(chart)
     }
 
     const pointSeries = seriesWebglPoint()
@@ -38,44 +53,76 @@
         .crossValue(d => d.x)
         .mainValue(d => d.y)
 
+    const annotationSeries = seriesSvgAnnotation().notePadding(15).type(annotationCallout)
+
     const _zoom = zoom()
         .scaleExtent([0.8, 20])
         .on("zoom", (e) => {
             console.log(e.transform)
-            console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`')
             xScale.domain(e.transform.rescaleX(xScaleOriginal).domain());
             yScale.domain(e.transform.rescaleY(yScaleOriginal).domain());
             redraw()
         })
 
+    const createAnnotationData = datapoint => ({
+        note: {
+            label: 'Description Text<br>Testing',
+            bgPadding: 5,
+            title: 'Title<sub>c</sub>'
+        },
+        x: datapoint.x,
+        y: datapoint.y,
+        dx: 20,
+        dy: 20
+    })
+
+    const _pointer = pointer().on("point", ([coord]) => {
+        // annotations.pop()
+        if (coord) {
+            // find the closes datapoint to the pointer
+            const x = xScale.invert(coord.x);
+            const y = yScale.invert(coord.y);
+            const radius = Math.abs(xScale.invert(coord.x) - xScale.invert(coord.x - 20));
+            const closestDatum = quadtree.find(x, y, radius);
+            console.log(closestDatum)
+
+            if (closestDatum) {
+                annotations[0] = createAnnotationData(closestDatum);
+            }
+        }
+        redraw()
+    })
+
     const chart = chartCartesian(xScale, yScale)
         .webglPlotArea(
             seriesWebglMulti().series([pointSeries]).mapping(d => d.filtered_data).key(d => d.id)
         )
+        .svgPlotArea(
+            // only render the annotations series on the SVG layer
+            seriesSvgMulti().series([annotationSeries]).mapping(d => d.annotations)
+        )
         .decorate(sel => {
-            sel.enter().select("d3fc-canvas.plot-area")
+            sel.enter().select("d3fc-svg.plot-area")
                 .on("measure.range", (e) => {
                     // Map to actual pixel values.
                     xScaleOriginal.range([0, e.detail.width]);
                     yScaleOriginal.range([e.detail.height, 0]);
                 })
                 .call(_zoom)
+                .call(_pointer)
         })
 
 
 
-    const setFills = () => {
-        const fillColor = webglFillColor().value(x => webglColor(x.color)).data(filtered_data)
-        pointSeries.decorate(program => fillColor(program))
-    }
-
     onMount(() => {
         data = points.p1
         filtered_data = applyFilters(data)
+        buildQuadTree()
         setFills()
         redraw()
     })
 
+    //// BUTTON FUNCTIONS
     const reset = () => {
         d3.select("d3fc-canvas.plot-area").transition().duration(2500).call(_zoom.transform, zoomIdentity)
     }
@@ -83,11 +130,13 @@
     const addP2 = () => {
         data = points.p1.concat(points.p2)
         filtered_data = applyFilters()
+        buildQuadTree()
         setFills()
         const transform = zoomIdentity.translate(-380, -63).scale(1.7)
         d3.select("d3fc-canvas.plot-area").transition().duration(2500).call(_zoom.transform, transform)
     }
 
+    //// UTIL FUNCTIONS
     const webglColor = color => {
         const {r, g, b, opacity} = d3.color(color).rgb();
         return [r / 255, g / 255, b / 255, opacity];
--- src/point-generator.js
+++ src/point-generator.js
@@ -5,12 +5,12 @@ const p2 = []
 
 let id = 0
 
-for (let i = 0; i < 2000; i++) {
+for (let i = 0; i < 200000; i++) {
     const x = Math.random() * 20
     const y = Math.random() * 20
     const tc = Math.random()
 
-    p1.push({id: id, x: x, tc: tc, y: y, size: 5, color: interpolateReds(Math.random())})
+    p1.push({id: id, x: x, tc: tc, y: y, size: 1, color: interpolateReds(Math.random())})
     id += 1
 }
 
@@ -19,7 +19,7 @@ for (let i = 0; i < 2000; i++) {
     const y = Math.random() * 20
     const tc = Math.random()
 
-    p2.push({id: id, tc: tc, x: x, y: y, size: 10, color: interpolateBlues(Math.random())})
+    p2.push({id: id, tc: tc, x: x, y: y, size: 25, color: interpolateBlues(Math.random())})
     id += 1
 }
 
